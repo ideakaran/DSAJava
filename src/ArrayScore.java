a87409fdbe04e17e91369cd9e3fc174c68d18d28
KaranPant <kpant321@gmail.com>
2022-10-23 16:29:54 +0545

    Initial work

---
+++ src/ArrayScore.java
@@ -0,0 +1,9 @@
+public class ArrayScore {
+    public static void main(String[] args) {
+        /**
+         * Given an array scores [][] = {“jerry”,”65”},{“bob”,”91”}, {“jerry”,”23”}, {“Eric”,”83”}} Find the student with highest average score
+         */
+
+        String arrScore[][] = {{"jerry", "65"}, {"bob", "91"}, {"jerry", "23"}, {"Eric", "83"}};
+    }
+}
